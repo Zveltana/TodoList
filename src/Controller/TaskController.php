308b84ac799c5fa8c56a1df13a57908d60e38ca9
Amélia Massot <ameliamassot@gmail.com>
2023-06-20 00:24:02 +0200

    Adding TodoList in project
    continue migration, adding crud for user and test
    
    Adding access for user role
    
    Adding creation task and modification it status
    
    Added consultation of completed tasks
    
    Modifying task buttons

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends AbstractController
+{
+    #[Route('/tasks', name: 'task_list')]
+    public function list(TaskRepository $taskRepository): Response
+    {
+        $task = $taskRepository->findAll();
+
+        return $this->render('task/list.html.twig', [
+            'tasks' => $task,
+        ]);
+    }
+
+    #[Route('/tasks/create', name: 'task_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task->setCreatedAt(new \DateTimeImmutable());
+            $em = $entityManager;
+
+            $em->persist($task);
+            $em->flush();
+
+            $this->addFlash('success', 'La tâche a été bien été ajoutée.');
+
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->renderForm('task/create.html.twig', ['form' => $form]);
+    }
+
+    #[Route('/tasks/finished', name: 'task_finished')]
+    public function done(TaskRepository $taskRepository): Response
+    {
+        $taskFinished = $taskRepository->findBy(['isDone' => 1]);
+
+        return $this->render('task/finished.html.twig', [
+            'tasksFinished' => $taskFinished,
+        ]);
+    }
+
+    #[Route('/tasks/unfinished', name: 'task_unfinished')]
+    public function unfinished(TaskRepository $taskRepository): Response
+    {
+        $taskUnfinished = $taskRepository->findBy(['isDone' => 0]);
+
+        return $this->render('task/unfinished.html.twig', [
+            'tasksUnfinished' => $taskUnfinished,
+        ]);
+    }
+
+    #[Route('/tasks/{id}/edit', name: 'task_edit')]
+    public function edit(Task $task, Request $request): Response
+    {
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->addFlash('success', 'La tâche a bien été modifiée.');
+
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->renderForm('task/edit.html.twig', [
+            'form' => $form,
+            'task' => $task,
+        ]);
+    }
+
+    #[Route('/tasks/{id}/toggle', name: 'task_toggle')]
+    public function toggle(Task $task, EntityManagerInterface $em): Response
+    {
+        if ($task->isIsDone() !== true) {
+            $task->setIsDone(true);
+            $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+        } else {
+            $task->setIsDone(false);
+            $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme non terminée.', $task->getTitle()));
+        }
+
+        $em->flush();
+
+        return $this->redirectToRoute('task_list');
+    }
+
+    #[Route('/tasks/{id}/delete', name: 'task_delete')]
+    public function delete(Task $task, EntityManagerInterface $em): Response
+    {
+        $em->remove($task);
+        $em->flush();
+
+        $this->addFlash('success', 'La tâche a bien été supprimée.');
+
+        return $this->redirectToRoute('task_list');
+    }
+}
